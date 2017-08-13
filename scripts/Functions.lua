3f545bdf0172f9d2008326f5cad45bfa06e9de7e
Chad <nshusa99@gmail.com>
2017-08-13 06:02:41 -0500

    Added vararg function

--- scripts/Functions.lua
+++ scripts/Functions.lua
@@ -1,11 +1,26 @@
+-- simple function with a parameter
 function sayHello(name)
     print("Hello " .. name .. "!")
 end
 
+-- function that returns a value
 function add(value1, value2)
     return value1 + value2
 end
 
+-- function with variable number of arguments
+function varargs(...)
+    local arg = {... }
+
+    for i, v in ipairs(arg) do
+        print(arg[i])
+    end
+end
+
+-- calling functions
+
 sayHello("Chad")
 
 print(add(2, 3))
+
+varargs("Chad", "Bob", "Sally")
